8ec809583d21fd1405ba188d0043bc19f7bfd808
DaveTobing <dave.matthew@ui.ac.id>
2022-09-21 10:26:45 +0700

    fixing username and hostname

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -12,7 +12,7 @@
 # You new to set "REC2" with your own Public-Key Identity!
 # Check it out with "gpg --list-key"
 # ####################### Replace REC2 ####
-REC2="7906855DDBA7C7E4"
+REC2="D9BDB4F2BD79FD26"
 # ####################### ####### #### ####
 # REC1: public key
 REC1="63FB12B215403B20"
